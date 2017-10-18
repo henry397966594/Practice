ba18e12d69be1608599adc2e366d22f22017a336
Henry <18618415652@163.com>
2017-10-18 11:40:35 +0800

    嘿嘿

---
+++ LearnVue/vue.md
@@ -0,0 +1,17 @@
+
+   # vue 的生命周期：钩子函数
+   ## create：=》实例已经创建
+   ## beforecompile:编译之前
+   ## compiled:编译之后
+   ## ready:文本节点插入到文档中
+   ## beforeDestroy:销毁之前
+   ## destory:销毁
+ # vue实例的简单方法
+ ```
+  vm.$el =就是元素
+  vm.$data->就是数据
+  vm.$mount->手动挂载
+  vm.$options->获取自定义属性
+  vm.$destroy->=销毁对象
+  vm.$log->查看现在的数据状态
+  ```
---
+++ practice.js
@@ -0,0 +1,127 @@
+//  function Timer(){
+//      this.s1=0;
+//      this.s2=0;
+//      setInterval(function(){
+//          this.s1++;
+//      }, 1000)
+//      setInterval(()=>this.s2++, 1000)
+//  }
+//  let timer = new Timer();
+//  setTimeout(()=> console.log('s1:',timer.s1),3100)
+//  setTimeout(()=> console.log('s2:',timer.s2),3100)
+// let insert = (value) => ({
+//     into: (array) => ({
+//         after: (afterValue) => {
+//             array.splice(array.indexOf(afterValue) + 1, 0, value);
+//             return array;
+//         }
+//     })
+// });
+
+// console.log(insert(2).into([1, 3]).after(1));
+//  //[1, 2, 3]
+// let set = new Set(['red', 'green', 'blue']);
+// let arr = [...set];
+
+// let arr = [3, 5, 2, 2, 5, 5];
+// let unique = [...new Set(arr)];
+// console.log(unique);
+// const myMap = new Map()
+// .set(true, 7)
+// .set({foo: 3}, ['abc']);
+// console.log([...myMap]);
+
+
+// [ [ true, 7 ], [ { foo: 3 }, [ 'abc' ] ] ]
+
+// const map = new Map([
+//   [1, 'one'],
+//   [2, 'two'],
+//   [3, 'three'],
+// ]);
+
+// console.log([...map.entries()]);
+
+// (function (){
+//     console.log('hello,world')
+// })()
+// 
+// var a = "5";
+// var b = "3";
+// if (a < b) // true
+//   console.log( (a + " is less than " + b));
+
+// else if (a > b)
+// console.log( (a + " is greater than " + b));
+
+// else
+// console.log((a + " and " + b + " are equal."));
+// console.log(Boolean(''));
+// console.log(Boolean('234'));
+// switch ('kkk') {
+//     case 'eat':
+//         console.log('hello world')
+//         break;
+//     case 'draw':
+//         console.log('fuck the world')
+//         break;
+//     default:
+//         console.log('去死吧')
+// }
+//键值对
+// !(function(){
+//     let day = new Date().getDay();
+//     switch (day) {
+//         case 0:console.log('tody is sunday');
+//         break;
+//         case 1:console.log('tody is mondsy');
+//         break;
+//         case 2:console.log('tody is tuesday');
+//         break;
+//         case 3:console.log('tody is wednesday');
+//         break;
+//         case 4:console.log('tody is thursday');
+//         break;
+//         case 5:console.log('tody is friday');
+//         break;
+//         case 6:console.log('tody is saturday');
+//         break;
+//     }
+// })()
+// for(var i = 0 ;i<3;i++){
+// setTimeout(function(){
+//     console.log(i)
+    
+// },(function(){
+//     var b=i*1000;
+//     console.log(b);
+//     return b
+// })());
+// }
+function Parent(hello){
+    this.hello = hello;
+  }
+  Parent.prototype.sayHello = function(){
+   console.log(this.hello);
+  }
+
+  function Child(hello,world){
+    Parent.call(this,hello);//将父类的属性继承过来
+    this.world = world;//新增一些属性
+  }
+
+  Child.prototype = new Parent();//将父类的方法继承过来
+
+  Child.prototype.sayWorld = function(){//新增一些方法
+    console.log(this.world);
+  }
+
+  var c = new Child("zhangsan","lisi");
+  c.sayHello();
+  c.sayWorld(); 
+
+
+
+
+
+
