45d76140c60bf6e32cded0cdcbf9f294a3f0e976
Henry <18618415652@163.com>
2017-10-20 02:05:23 +0800

    练习

--- practice.js
+++ practice.js
@@ -98,27 +98,39 @@
 //     return b
 // })());
 // }
-function Parent(hello){
-    this.hello = hello;
-  }
-  Parent.prototype.sayHello = function(){
-   console.log(this.hello);
-  }
+// function Parent(hello){
+//     this.hello = hello;
+//   }
+//   Parent.prototype.sayHello = function(){
+//    console.log(this.hello);
+//   }
 
-  function Child(hello,world){
-    Parent.call(this,hello);//将父类的属性继承过来
-    this.world = world;//新增一些属性
-  }
+//   function Child(hello,world){
+//     Parent.call(this,hello);//将父类的属性继承过来
+//     this.world = world;//新增一些属性
+//   }
 
-  Child.prototype = new Parent();//将父类的方法继承过来
+//   Child.prototype = new Parent();//将父类的方法继承过来
 
-  Child.prototype.sayWorld = function(){//新增一些方法
-    console.log(this.world);
-  }
+//   Child.prototype.sayWorld = function(){//新增一些方法
+//     console.log(this.world);
+//   }
 
-  var c = new Child("zhangsan","lisi");
-  c.sayHello();
-  c.sayWorld(); 
+//   var c = new Child("zhangsan","lisi");
+//   c.sayHello();
+//   c.sayWorld(); 
+var name = 'window';
+var Tom = {
+name:'Tom',
+show:function(){
+ console.log(this.name);
+},
+wait:function(){
+ var fun = this.show;
+ fun();
+}
+};
+Tom.wait();
 
 
 
